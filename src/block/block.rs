6375278f1a29e2ca9b256c15e315bc9fced2ad88
tolumide-ng <tolumideshopein@gmail.com>
2021-05-23 23:21:36 +0100

    nonce mining

--- src/block/block.rs
+++ src/block/block.rs
@@ -1,6 +1,6 @@
 use std::fmt::{self, Debug, Formatter};
 
-use crate::{u128_bytes, u32_bytes, u64_bytes, BlockHash, HashHable};
+use crate::{difficulty_bytes_as_u128, u128_bytes, u32_bytes, u64_bytes, BlockHash, HashHable};
 
 pub struct Block {
     pub index: u32,
@@ -9,17 +9,19 @@ pub struct Block {
     pub prev_block_hash: BlockHash,
     pub nonce: u64,
     pub payload: String,
+    pub difficulty: u128,
 }
 
 impl Debug for Block {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(
             f,
-            "Block[{}]: {} at: {} with: {}",
+            "Block[{}]: {} at: {} with: {}  and nonce {}",
             &self.index,
             hex::encode(&self.hash),
             &self.timestamp,
-            &self.payload
+            &self.payload,
+            &self.nonce
         )
     }
 }
@@ -31,6 +33,7 @@ impl Block {
         prev_block_hash: BlockHash,
         nonce: u64,
         payload: String,
+        difficulty: u128,
     ) -> Self {
         Block {
             index,
@@ -39,6 +42,19 @@ impl Block {
             nonce,
             payload,
             hash: vec![0; 32],
+            difficulty,
+        }
+    }
+
+    pub fn mine(&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
         }
     }
 }
@@ -49,10 +65,15 @@ impl HashHable for Block {
 
         bytes.extend(&u32_bytes(&self.index));
         bytes.extend(&u128_bytes(&self.timestamp));
+        bytes.extend(&u128_bytes(&self.difficulty));
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
         bytes.extend(self.payload.as_bytes());
 
         return bytes;
     }
+}
+
+pub fn check_difficulty(hash: &BlockHash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(hash)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,21 @@ use blockchain;
 use blockchain::{Block, HashHable};
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis Block".to_string());
+    let mut block = Block::new(
+        0,
+        0,
+        vec![0; 32],
+        0,
+        "Genesis Block".to_string(),
+        0x00ffffffffffffffffffffffffffff,
+    );
 
-    let h = block.hash();
+    block.hash = block.hash();
 
-    block.hash = h;
+    println!("{:#?}", &block);
 
-    println!("{:#?}", block);
+    block.mine();
+
+    println!("{:#?}", &block);
     // println!("the hash {:?}", &h);
 }
